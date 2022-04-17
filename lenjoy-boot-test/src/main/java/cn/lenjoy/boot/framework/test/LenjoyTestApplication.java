51cca91c6c14012b1e9517ec2ac349978bc8c755
bincloud <bincloud@yeah.net>
2022-04-17 21:18:15 +0800

    提交类型：init 提交主题：init lenjoy-spring-boot-starter-captcha 图形验证码 提交内容： 1.lenjoy-spring-boot-starter-captcha init

---
+++ lenjoy-boot-test/src/main/java/cn/lenjoy/boot/framework/test/LenjoyTestApplication.java
@@ -0,0 +1,17 @@
+package cn.lenjoy.boot.framework.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: lenjoy's bincloud,mvpzhou
+ * @date: Create By lenjoy's bincloud,mvpzhou on 2022 04 17 星期日
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class LenjoyTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LenjoyTestApplication.class, args);
+    }
+}
