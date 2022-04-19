11e0d3c6adea449a881e3dc3c2be2b693fd8f2f5
bincloud <bincloud@yeah.net>
2022-04-19 21:24:10 +0800

    提交类型：feat 提交主题：init lenjoy-application 模块 提交内容： 1.lenjoy-application

---
+++ lenjoy-application/src/main/java/cn/lenjoy/boot/application/LenjoyBootApplication.java
@@ -0,0 +1,11 @@
+package cn.lenjoy.boot.application;
+
+/**
+ * @description: lenjoy application 引导类
+ * @author: lenjoy's bincloud,mvpzhou
+ * @date: Create By lenjoy's bincloud,mvpzhou on 2022 04 19 星期二
+ * @version: 1.0.0
+ */
+
+public class LenjoyBootApplication {
+}
---
+++ lenjoy-system/lenjoy-system-service/src/main/java/cn/lenjoy/boot/system/controller/auth/AuthController.java
@@ -0,0 +1,10 @@
+package cn.lenjoy.boot.system.controller.auth;
+
+/**
+ * @description: 认证相关接口
+ * @author: lenjoy's bincloud,mvpzhou
+ * @date: Create By lenjoy's bincloud,mvpzhou on 2022 04 19 星期二
+ * @version: 1.0.0
+ */
+public class AuthController {
+}
